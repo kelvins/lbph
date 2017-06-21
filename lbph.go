b7ce77f6af76ab215c0bfedecf28e1d6b06b13a6
Kelvin S. Prado <kelvinpfw@hotmail.com>
2017-06-20 22:42:53 -0300

    Creating the histograms

--- lbph.go
+++ lbph.go
@@ -2,24 +2,28 @@
 package lbph
 
 import (
+	"errors"
+	"fmt"
 	"image"
-  "errors"
 	_ "image/gif"
-	_ "image/png"
 	_ "image/jpeg"
+	_ "image/png"
+	"strconv"
 
 	"github.com/kelvins/imgproc"
 )
 
+var histograms [][]int16
+
 func checkInputData(images []image.Image) error {
-  // Get the image bounds
+	// Get the image bounds
 	bounds := images[0].Bounds()
 	width, height := bounds.Max.X, bounds.Max.Y
 
 	for index := 0; index < len(images); index++ {
 		// Check if the image is in grayscale
 		if !imgproc.IsGrayscale(images[index]) {
-  		return errors.New("One or more images are not in grayscale")
+			return errors.New("One or more images are not in grayscale")
 		}
 
 		b := images[index].Bounds()
@@ -29,7 +33,15 @@ func checkInputData(images []image.Image) error {
 			return errors.New("One or more images have different sizes")
 		}
 	}
-  return nil
+	return nil
+}
+
+func getBinary(value, threshold int) string {
+	if value >= threshold {
+		return "1"
+	} else {
+		return "0"
+	}
 }
 
 // Function used to train the algorithm
@@ -42,11 +54,58 @@ func Train(images []image.Image, labels []string) error {
 		return errors.New("Empty vector")
 	}
 
-  // Check if the input data is in the correct format
-  err := checkInputData(images)
-  if err != nil {
-    return err
-  }
+	// Check if the input data is in the correct format
+	err := checkInputData(images)
+	if err != nil {
+		return err
+	}
+
+	// STEP 2 - Convert the image
+	for index := 0; index < len(images); index++ {
+		bounds := images[index].Bounds()
+		w, h := bounds.Max.X, bounds.Max.Y
+		image := image.NewGray(bounds)
+
+		// Convert each pixel to grayscale
+		for x := 0; x < w; x++ {
+			for y := 0; y < h; y++ {
+				image.Set(x, y, images[index].At(x, y))
+			}
+		}
+
+		var histogram []int16
+		// Convert each pixel to grayscale
+		for row := 1; row < w-1; row++ {
+			for col := 1; col < h-1; col++ {
+
+				threshold := image.GrayAt(row, col).Y
+
+				binaryResult := ""
+				for r := row - 1; r <= row+1; r++ {
+					for c := col - 1; c <= col+1; c++ {
+						if r != row || c != col {
+							binaryResult += getBinary(image.GrayAt(r, c).Y, threshold)
+						}
+					}
+				}
+
+				i, err := strconv.ParseInt(binaryResult, 10, 32)
+				if err != nil {
+					return errors.New("Error normalizing the images")
+				} else {
+					fmt.Println(i)
+					histogram = append(histogram, i)
+				}
+			}
+		}
+
+		histograms = append(histograms, histogram)
+	}
+
+	// This conditional must never occurs
+	if len(histograms) == 0 {
+		return errors.New("None histogram was calculated")
+	}
 
 	return nil
 }
