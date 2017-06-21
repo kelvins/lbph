d134e8863d93db58c5fca75ce0f2e752e87451d8
Kelvin S. Prado <kelvinpfw@hotmail.com>
2017-06-20 21:14:22 -0300

    First commit :tada:

---
+++ lbph.go
@@ -0,0 +1,14 @@
+// Performs face recognition using lbph
+package lbph
+
+import (
+	"image"
+)
+
+func Train(images []image.Image, labels []string) error {
+	return nil
+}
+
+func Predict(image image.Image) (string, float64, error) {
+	return "", 0.0, nil
+}
