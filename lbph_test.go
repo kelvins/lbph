0a0fd7dd3ae73910385683d2aa345d2277a5b265
Kelvin S. Prado <kelvinpfw@hotmail.com>
2017-06-22 21:24:25 -0300

    Added more tests

--- lbph_test.go
+++ lbph_test.go
@@ -57,4 +57,60 @@ func TestCheckInputData(t *testing.T) {
 	if err == nil {
 		t.Error("Expected: Images have different sizes. Received: nil")
 	}
+	images = nil
+
+	// No error
+	img, err = loadImage("./test/1.png")
+	if err != nil {
+		t.Error(err)
+	}
+	images = append(images, img)
+	err = checkInputData(images)
+	if err != nil {
+		t.Error("Expected: nil. Received: ", err)
+	}
+}
+
+func TestPredict(t *testing.T) {
+
+	var paths []string
+	paths = append(paths, "./train/1.png")
+	paths = append(paths, "./train/2.png")
+	paths = append(paths, "./train/3.png")
+
+	var labels []string
+	labels = append(labels, "1")
+	labels = append(labels, "2")
+	labels = append(labels, "3")
+
+	var images []image.Image
+
+	for index := 0; index < len(paths); index++ {
+		img, err := loadImage(paths[index])
+		if err != nil {
+			t.Error(err)
+		}
+		images = append(images, img)
+	}
+
+	err := Train(images, labels)
+	if err != nil {
+		t.Error(err)
+	}
+
+	img, err := loadImage("./test/1.png")
+	if err != nil {
+		t.Error(err)
+	}
+
+	lbl, dist, err := Predict(img)
+	if err != nil {
+		t.Error(err)
+	}
+	if lbl != "3" {
+		t.Error("Expected label == 3 - Received",lbl)
+	}
+	if dist > 500 || dist < 0 {
+		t.Error("Received dist : ",dist)
+	}
 }
