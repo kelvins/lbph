2b20c745e352e06b92ab1f123a21f370365f25a5
Kelvin Salton do Prado <kelvinpfw@hotmail.com>
2017-07-09 10:54:33 -0300

    Testing the GetHistogramDist function

--- histogram/histogram_test.go
+++ histogram/histogram_test.go
@@ -4,6 +4,41 @@ import (
 	"testing"
 )
 
-func TestIsGrayscale(t *testing.T) {
+var EPSILON float64 = 0.00000001
+func floatEquals(a, b float64) bool {
+	if (a - b) < EPSILON && (b - a) < EPSILON {
+		return true
+	}
+	return false
+}
 
+func TestGetHistogramDist(t *testing.T) {
+	var hist1 [256]int64
+	var hist2 [256]int64
+
+	hist1[0] = 15
+	hist2[0] = 5
+	hist1[255] = 50
+	hist2[255] = 40
+
+	dist := GetHistogramDist(hist1, hist2)
+	if !floatEquals(dist, 14.142135623730951) {
+		t.Error(
+			"Expected distance:", 14.142135623730951,
+			"Received distance:", dist,
+		)
+	}
+
+	hist1[0] = 15
+	hist2[0] = 15
+	hist1[255] = 50
+	hist2[255] = 50
+
+	dist = GetHistogramDist(hist1, hist2)
+	if !floatEquals(dist, 0.0) {
+		t.Error(
+			"Expected distance:", 0.0,
+			"Received distance:", dist,
+		)
+	}
 }
