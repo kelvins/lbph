6354722eb95c6dfab76a98207af92e50997189c0
Kelvin S. Prado <kelvinpfw@hotmail.com>
2017-08-01 22:41:38 -0300

    Changed lbp_test to use assert

--- lbp/lbp_test.go
+++ lbp/lbp_test.go
@@ -2,36 +2,30 @@ package lbp
 
 import (
 	"testing"
+
 	"github.com/kelvins/lbph/common"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestApplyLBP(t *testing.T) {
 	img, err := common.LoadImage("../dataset/test/4.png")
-	if err != nil {
-		t.Error(err)
-	}
+	assert.Nil(t, err)
 
 	// Results manually calculated (radius:1 - neighbors:8)
 	var expectedLBP [][]uint8
-	expectedLBP = append(expectedLBP, []uint8{ 91, 190,  93, 179})
+	expectedLBP = append(expectedLBP, []uint8{91, 190, 93, 179})
 	expectedLBP = append(expectedLBP, []uint8{238, 245, 255, 206})
 	expectedLBP = append(expectedLBP, []uint8{115, 255, 175, 119})
 	expectedLBP = append(expectedLBP, []uint8{205, 186, 125, 218})
 
 	pixels, err := ApplyLBP(img, 1, 8)
-	if err != nil {
-		t.Error(err)
-	}
+	assert.Nil(t, err)
 
 	// Check each pixel
 	for x := 0; x < len(pixels); x++ {
 		for y := 0; y < len(pixels[x]); y++ {
-			if pixels[x][y] != expectedLBP[x][y] {
-				t.Error(
-					"Expected value: ", expectedLBP[x][y],
-					"Received value: ", pixels[x][y],
-				)
-			}
+			assert.Equal(t, pixels[x][y], expectedLBP[x][y], "The pixel value should be equal")
 		}
 	}
 }
