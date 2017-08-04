fab748b6e68d7c324ec7496fba3c5e1b1a7bb605
Kelvin S. Prado <kelvinpfw@hotmail.com>
2017-08-04 11:34:38 -0300

    Testing EuclideanDistance and NormalizedEuclideanDistance

--- math/math_test.go
+++ math/math_test.go
@@ -1,1 +1,51 @@
 package math
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEuclideanDistance(t *testing.T) {
+	// Table tests
+	var tTable = []struct {
+		hist1  []uint8
+		hist2  []uint8
+		result float64
+	}{
+		{[]uint8{1,2,3,4,5,6,7,8,9}, []uint8{9,8,7,6,5,4,3,2,1}, 15.491933384829668},
+		{[]uint8{1,1,1,1,1,1,1,1,1}, []uint8{9,9,9,9,9,9,9,9,9}, 24.0},
+		{[]uint8{9,9,9,9,9,9,9,9,9}, []uint8{1,1,1,1,1,1,1,1,1}, 24.0},
+		{[]uint8{9,9,9,9,9,9,9,9,9}, []uint8{9,9,9,9,9,9,9,9,9}, 0.0},
+		{[]uint8{1,4,5,4,1,0,0,0,0}, []uint8{0,0,0,0,0,1,3,4,2}, 9.433981132056603},
+	}
+
+	// Test with all values in the table
+	for _, pair := range tTable {
+		distance, err := EuclideanDistance(pair.hist1, pair.hist2)
+		assert.Nil(t, err)
+		assert.Equal(t, pair.result, distance)
+	}
+}
+
+func TestNormalizedEuclideanDistance(t *testing.T) {
+	// Table tests
+	var tTable = []struct {
+		hist1  []uint8
+		hist2  []uint8
+		result float64
+	}{
+		{[]uint8{1,2,3,4,5,6,7,8,9}, []uint8{9,8,7,6,5,4,3,2,1}, 5.163977794943222},
+		{[]uint8{1,1,1,1,1,1,1,1,1}, []uint8{9,9,9,9,9,9,9,9,9}, 8.0},
+		{[]uint8{9,9,9,9,9,9,9,9,9}, []uint8{1,1,1,1,1,1,1,1,1}, 8.0},
+		{[]uint8{9,9,9,9,9,9,9,9,9}, []uint8{9,9,9,9,9,9,9,9,9}, 0.0},
+		{[]uint8{1,4,5,4,1,0,0,0,0}, []uint8{0,0,0,0,0,1,3,4,2}, 3.144660377352201},
+	}
+
+	// Test with all values in the table
+	for _, pair := range tTable {
+		distance, err := NormalizedEuclideanDistance(pair.hist1, pair.hist2)
+		assert.Nil(t, err)
+		assert.Equal(t, pair.result, distance)
+	}
+}
