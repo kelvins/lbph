37ca273d207c4eca386e16b5ff085002f12257ae
Kelvin S. Prado <kelvinpfw@hotmail.com>
2017-08-04 23:12:55 -0300

    Testing compare histograms functions

--- math/math.go
+++ math/math.go
@@ -1,8 +1,8 @@
 package math
 
 import (
-	"math"
 	"errors"
+	"math"
 )
 
 // min function returns the minimum value.
@@ -21,6 +21,26 @@ func sum(slice []uint8) float64 {
 		sum += float64(value)
 	}
 	return sum
+}
+
+// chiSquare calculates the distance between two histograms using
+// the chi square statistic.
+// References:
+// http://file.scirp.org/Html/8-72278_30995.htm
+// https://www.google.com/patents/WO2007080817A1?cl=en
+func ChiSquare(hist1, hist2 []uint8) (float64, error) {
+	// Check the histogram sizes
+	if len(hist1) != len(hist2) {
+		return 0, errors.New("Could not compare the histograms. The slices have different sizes.")
+	}
+
+	var sum float64
+	for index := 0; index < len(hist1); index++ {
+		numerator := float64((hist1[index] - hist2[index]) * (hist1[index] - hist2[index]))
+		denominator := float64(hist1[index])
+		sum += numerator / denominator
+	}
+	return sum, nil
 }
 
 // EuclideanDistance calculates the euclidean distance between two histograms
@@ -51,7 +71,7 @@ func NormalizedEuclideanDistance(hist1, hist2 []uint8) (float64, error) {
 
 	var sum float64
 	for index := 0; index < len(hist1); index++ {
-		sum += float64((hist1[index] - hist2[index]) * (hist1[index] - hist2[index])) / float64(len(hist1))
+		sum += float64((hist1[index]-hist2[index])*(hist1[index]-hist2[index])) / float64(len(hist1))
 	}
 	return math.Sqrt(sum), nil
 }
@@ -63,7 +83,7 @@ func NormalizedEuclideanDistance(hist1, hist2 []uint8) (float64, error) {
 // http://blog.datadive.net/histogram-intersection-for-change-detection/
 // https://dsp.stackexchange.com/questions/18065/histogram-intersection-with-two-different-bin-sizes
 // https://mpatacchiola.github.io/blog/2016/11/12/the-simplest-classifier-histogram-intersection.html
-func Intersection(hist1, hist2 []uint8) (float64, error)  {
+func Intersection(hist1, hist2 []uint8) (float64, error) {
 	// Check the histogram sizes
 	if len(hist1) != len(hist2) {
 		return 0, errors.New("Could not compare the histograms. The slices have different sizes.")
@@ -81,51 +101,20 @@ func Intersection(hist1, hist2 []uint8) (float64, error)  {
 // References:
 // https://dsp.stackexchange.com/questions/18065/histogram-intersection-with-two-different-bin-sizes
 // https://mpatacchiola.github.io/blog/2016/11/12/the-simplest-classifier-histogram-intersection.html
-func NormalizedIntersection(hist1, hist2 []uint8) (float64, error)  {
+func NormalizedIntersection(hist1, hist2 []uint8) (float64, error) {
 	// Check the histogram sizes
 	if len(hist1) != len(hist2) {
 		return 0, errors.New("Could not compare the histograms. The slices have different sizes.")
 	}
-	//sum1 := sum(hist1)
-	//sum2 := sum(hist2)
-	/*var mini float64
-	if sum1 < sum2 {
-		mini = sum1
-	} else {
-		mini = sum2
-	}*/
 
-	var inter float64
-	for index := 0; index < len(hist1); index++ {
-		inter += float64(min(hist1[index], hist2[index]))
-	}
+	intersection, _ := Intersection(hist1, hist2)
 
 	sum1 := sum(hist1)
 	sum2 := sum(hist2)
 
 	if sum1 > sum2 {
-		return inter/sum1, nil
+		return intersection / sum1, nil
 	} else {
-		return inter/sum2, nil
+		return intersection / sum2, nil
 	}
-}
-
-// chiSquare calculates the distance between two histograms using
-// the chi square statistic.
-// References:
-// http://file.scirp.org/Html/8-72278_30995.htm
-// https://www.google.com/patents/WO2007080817A1?cl=en
-func ChiSquare(hist1, hist2 []uint8) (float64, error)  {
-	// Check the histogram sizes
-	if len(hist1) != len(hist2) {
-		return 0, errors.New("Could not compare the histograms. The slices have different sizes.")
-	}
-
-	var sum float64
-	for index := 0; index < len(hist1); index++ {
-		numerator := float64((hist1[index] - hist2[index]) * (hist1[index] - hist2[index]))
-		denominator := float64(hist1[index] + hist2[index])
-		sum += numerator / denominator
-	}
-	return sum, nil
 }
--- math/math_test.go
+++ math/math_test.go
@@ -1,51 +1,122 @@
 package math
 
 import (
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestEuclideanDistance(t *testing.T) {
+func TestChiSquare(t *testing.T) {
 	// Table tests
 	var tTable = []struct {
-		hist1  []uint8
-		hist2  []uint8
-		result float64
+		hist1      []uint8
+		hist2      []uint8
+		confidence float64
 	}{
-		{[]uint8{1,2,3,4,5,6,7,8,9}, []uint8{9,8,7,6,5,4,3,2,1}, 15.491933384829668},
-		{[]uint8{1,1,1,1,1,1,1,1,1}, []uint8{9,9,9,9,9,9,9,9,9}, 24.0},
-		{[]uint8{9,9,9,9,9,9,9,9,9}, []uint8{1,1,1,1,1,1,1,1,1}, 24.0},
-		{[]uint8{9,9,9,9,9,9,9,9,9}, []uint8{9,9,9,9,9,9,9,9,9}, 0.0},
-		{[]uint8{1,4,5,4,1,0,0,0,0}, []uint8{0,0,0,0,0,1,3,4,2}, 9.433981132056603},
+		{[]uint8{1, 2, 3, 4, 5, 6, 7, 8, 9}, []uint8{9, 8, 7, 6, 5, 4, 3, 2, 1}, 102.8968253968254},
+		{[]uint8{1, 1, 1, 1, 1, 1, 1, 1, 1}, []uint8{9, 9, 9, 9, 9, 9, 9, 9, 9}, 576.0},
+		{[]uint8{9, 9, 9, 9, 9, 9, 9, 9, 9}, []uint8{1, 1, 1, 1, 1, 1, 1, 1, 1}, 64.00000000000001},
+		{[]uint8{9, 9, 9, 9, 9, 9, 9, 9, 9}, []uint8{9, 9, 9, 9, 9, 9, 9, 9, 9}, 0.0},
+		{[]uint8{1, 4, 5, 4, 1, 0, 0, 0, 0}, []uint8{0, 0, 0, 0, 0, 1, 3, 4, 2}, -1.0},
 	}
 
 	// Test with all values in the table
 	for _, pair := range tTable {
-		distance, err := EuclideanDistance(pair.hist1, pair.hist2)
+		confidence, err := ChiSquare(pair.hist1, pair.hist2)
 		assert.Nil(t, err)
-		assert.Equal(t, pair.result, distance)
+		if pair.confidence >= 0 {
+			assert.Equal(t, pair.confidence, confidence)
+		} else {
+			assert.Equal(t, true, math.IsInf(confidence, 0))
+		}
+	}
+}
+
+func TestEuclideanDistance(t *testing.T) {
+	// Table tests
+	var tTable = []struct {
+		hist1      []uint8
+		hist2      []uint8
+		confidence float64
+	}{
+		{[]uint8{1, 2, 3, 4, 5, 6, 7, 8, 9}, []uint8{9, 8, 7, 6, 5, 4, 3, 2, 1}, 15.491933384829668},
+		{[]uint8{1, 1, 1, 1, 1, 1, 1, 1, 1}, []uint8{9, 9, 9, 9, 9, 9, 9, 9, 9}, 24.0},
+		{[]uint8{9, 9, 9, 9, 9, 9, 9, 9, 9}, []uint8{1, 1, 1, 1, 1, 1, 1, 1, 1}, 24.0},
+		{[]uint8{9, 9, 9, 9, 9, 9, 9, 9, 9}, []uint8{9, 9, 9, 9, 9, 9, 9, 9, 9}, 0.0},
+		{[]uint8{1, 4, 5, 4, 1, 0, 0, 0, 0}, []uint8{0, 0, 0, 0, 0, 1, 3, 4, 2}, 9.433981132056603},
+	}
+
+	// Test with all values in the table
+	for _, pair := range tTable {
+		confidence, err := EuclideanDistance(pair.hist1, pair.hist2)
+		assert.Nil(t, err)
+		assert.Equal(t, pair.confidence, confidence)
 	}
 }
 
 func TestNormalizedEuclideanDistance(t *testing.T) {
 	// Table tests
 	var tTable = []struct {
-		hist1  []uint8
-		hist2  []uint8
-		result float64
+		hist1      []uint8
+		hist2      []uint8
+		confidence float64
 	}{
-		{[]uint8{1,2,3,4,5,6,7,8,9}, []uint8{9,8,7,6,5,4,3,2,1}, 5.163977794943222},
-		{[]uint8{1,1,1,1,1,1,1,1,1}, []uint8{9,9,9,9,9,9,9,9,9}, 8.0},
-		{[]uint8{9,9,9,9,9,9,9,9,9}, []uint8{1,1,1,1,1,1,1,1,1}, 8.0},
-		{[]uint8{9,9,9,9,9,9,9,9,9}, []uint8{9,9,9,9,9,9,9,9,9}, 0.0},
-		{[]uint8{1,4,5,4,1,0,0,0,0}, []uint8{0,0,0,0,0,1,3,4,2}, 3.144660377352201},
+		{[]uint8{1, 2, 3, 4, 5, 6, 7, 8, 9}, []uint8{9, 8, 7, 6, 5, 4, 3, 2, 1}, 5.163977794943222},
+		{[]uint8{1, 1, 1, 1, 1, 1, 1, 1, 1}, []uint8{9, 9, 9, 9, 9, 9, 9, 9, 9}, 8.0},
+		{[]uint8{9, 9, 9, 9, 9, 9, 9, 9, 9}, []uint8{1, 1, 1, 1, 1, 1, 1, 1, 1}, 8.0},
+		{[]uint8{9, 9, 9, 9, 9, 9, 9, 9, 9}, []uint8{9, 9, 9, 9, 9, 9, 9, 9, 9}, 0.0},
+		{[]uint8{1, 4, 5, 4, 1, 0, 0, 0, 0}, []uint8{0, 0, 0, 0, 0, 1, 3, 4, 2}, 3.144660377352201},
 	}
 
 	// Test with all values in the table
 	for _, pair := range tTable {
-		distance, err := NormalizedEuclideanDistance(pair.hist1, pair.hist2)
+		confidence, err := NormalizedEuclideanDistance(pair.hist1, pair.hist2)
 		assert.Nil(t, err)
-		assert.Equal(t, pair.result, distance)
+		assert.Equal(t, pair.confidence, confidence)
+	}
+}
+
+func TestIntersection(t *testing.T) {
+	// Table tests
+	var tTable = []struct {
+		hist1      []uint8
+		hist2      []uint8
+		confidence float64
+	}{
+		{[]uint8{1, 2, 3, 4, 5, 6, 7, 8, 9}, []uint8{9, 8, 7, 6, 5, 4, 3, 2, 1}, 25},
+		{[]uint8{1, 1, 1, 1, 1, 1, 1, 1, 1}, []uint8{9, 9, 9, 9, 9, 9, 9, 9, 9}, 9.0},
+		{[]uint8{9, 9, 9, 9, 9, 9, 9, 9, 9}, []uint8{1, 1, 1, 1, 1, 1, 1, 1, 1}, 9.0},
+		{[]uint8{9, 9, 9, 9, 9, 9, 9, 9, 9}, []uint8{9, 9, 9, 9, 9, 9, 9, 9, 9}, 81.0},
+		{[]uint8{1, 4, 5, 4, 1, 0, 0, 0, 0}, []uint8{0, 0, 0, 0, 0, 1, 3, 4, 2}, 0.0},
+	}
+
+	// Test with all values in the table
+	for _, pair := range tTable {
+		confidence, err := Intersection(pair.hist1, pair.hist2)
+		assert.Nil(t, err)
+		assert.Equal(t, pair.confidence, confidence)
+	}
+}
+
+func TestNormalizedIntersection(t *testing.T) {
+	// Table tests
+	var tTable = []struct {
+		hist1      []uint8
+		hist2      []uint8
+		confidence float64
+	}{
+		{[]uint8{1, 2, 3, 4, 5, 6, 7, 8, 9}, []uint8{9, 8, 7, 6, 5, 4, 3, 2, 1}, 0.5555555555555556},
+		{[]uint8{1, 1, 1, 1, 1, 1, 1, 1, 1}, []uint8{9, 9, 9, 9, 9, 9, 9, 9, 9}, 0.1111111111111111},
+		{[]uint8{9, 9, 9, 9, 9, 9, 9, 9, 9}, []uint8{1, 1, 1, 1, 1, 1, 1, 1, 1}, 0.1111111111111111},
+		{[]uint8{9, 9, 9, 9, 9, 9, 9, 9, 9}, []uint8{9, 9, 9, 9, 9, 9, 9, 9, 9}, 1.0},
+		{[]uint8{1, 4, 5, 4, 1, 0, 0, 0, 0}, []uint8{0, 0, 0, 0, 0, 1, 3, 4, 2}, 0.0},
+	}
+
+	// Test with all values in the table
+	for _, pair := range tTable {
+		confidence, err := NormalizedIntersection(pair.hist1, pair.hist2)
+		assert.Nil(t, err)
+		assert.Equal(t, pair.confidence, confidence)
 	}
 }
